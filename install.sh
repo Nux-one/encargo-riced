c7c1c6f406c0618dac78182dab0893d633582890
Lorblak <kvasquez@fi.uba.ar>
2021-08-07 21:40:08 -0300

    refactor(install.sh): script was changed

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 USER_CONFIG_DIR="$HOME/.config/"
 
+# delete config files
+rm -rf "$USER_CONFIG_DIR"rofi
+rm -rf "$USER_CONFIG_DIR"polybar
+rm -rf "$USER_CONFIG_DIR"sxhkd
+rm -rf "$USER_CONFIG_DIR"bspwm
+rm -rf "$USER_CONFIG_DIR"picom
+
 # install config files in user config dir & fonts
 cp -r .fonts rofi polybar sxhkd bspwm picom "$USER_CONFIG_DIR"
 # reload sxhkd
