0024b713c249e368b26f4ead61cf4c30c1917e02
Lorblak <kvasquez@fi.uba.ar>
2021-08-28 18:24:36 -0300

    feat(theme_1/polybar): clickable launcher was added in theme

--- theme_1/polybar/launch.sh
+++ theme_1/polybar/launch.sh
@@ -8,6 +8,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
+polybar -q menu-bar -c "$DIR"/config.ini &
 polybar -q left-bar -c "$DIR"/config.ini &
 polybar -q middle-bar -c "$DIR"/config.ini &
 polybar -q right-bar -c "$DIR"/config.ini &
